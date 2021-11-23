5d2af438cd0a531e0d2dba24bdbd484397254ada
Pranay Jain <jpranay688@gmail.com>
2021-11-24 01:00:16 +0530

    add brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,6 +1,26 @@
 import './brand.css'
+import { google, slack, dropbox, atlassian, shopify } from './import'
+
 const Brand = () => {
-	return <div>Brand</div>
+	return (
+		<div className='gpt3__brand section__padding'>
+			<div>
+				<img src={google} alt='google logo' />
+			</div>
+			<div>
+				<img src={slack} alt='slack logo' />
+			</div>
+			<div>
+				<img src={atlassian} alt='atlassian logo' />
+			</div>
+			<div>
+				<img src={dropbox} alt='dropbox logo' />
+			</div>
+			<div>
+				<img src={shopify} alt='shopify logo' />
+			</div>
+		</div>
+	)
 }
 
 export default Brand
---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import google from '../../images/google.png'
+import slack from '../../images/slack.png'
+import dropbox from '../../images/dropbox.png'
+import atlassian from '../../images/atlassian.png'
+import shopify from '../../images/shopify.png'
+
+export { google, slack, dropbox, atlassian, shopify }
